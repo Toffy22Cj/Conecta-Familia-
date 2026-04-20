26b775701ff63012e19cff004337b79e3778630e
Carlos <cjpp12220@gmail.com>
2026-04-20 14:41:11 -0500

    primer

---
+++ backend/src/main/java/com/conectafamilia/backend/ConectaFamiliaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.conectafamilia.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaFamiliaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaFamiliaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/conectafamilia/backend/ConectaFamiliaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conectafamilia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaFamiliaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
